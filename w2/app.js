446f0606078b16f487bd49ca47bf97bb1512bac4
Chase Olisa <chaseolisa@gmail.com>
2026-01-22 05:29:35 -0800

    Step 2.2 Complete

--- w2/app.js
+++ w2/app.js
@@ -6,8 +6,8 @@ const totalDisplayElement = document.getElementById("total-display");
 const addItemButton = document.getElementById("add-item-btn");
 
 //These variables will change as the user interacts with the page.
-let userName = 'Chase';
-let clickCount = 0;
+let totalCost = 0;
+const itemPrice = 15;
 
 // --- Part 2: Define and call the handleButtonClick Function---
 // A function is a block of code designed to perform a particular task.
@@ -15,14 +15,14 @@ let clickCount = 0;
 const handleButtonClick = function() {
     //clickCount = clickCount + 1;
     // Increase clickCount by 1 each time the button is clicked
-    clickCount += 1;
+    totalCost += 1;
 
     // Template strings (literal) to easily combine our variables and text into one message
-    let message = `Hello, ${userName}! You have clicked the button ${clickCount} time(s).`;
+    let message = `Hello, ${totalCost}! You have clicked the button ${totalCost} time(s).`;
 
     // This is basic decision-making in JavaScript!
     // Use a simple 'if' statement to make our page react differently based on clickCount.
-    if(clickCount >= 5) {
+    if(totalCost >= 5) {
         // We can even change the style of an HTML element directly with JavaScript!
         // Change text color
         message += ' WOW, you are super clicker!';
@@ -34,7 +34,7 @@ const handleButtonClick = function() {
     // This is how JavaScript makes changes visible on the web page!
     totalDisplayElement.textContent = message;
 
-    console.log(`Button Clicked! Current click count: ${clickCount}`)
+    console.log(`Button Clicked! Current click count: ${totalCost}`)
 };
 
 
@@ -47,5 +47,5 @@ document.addEventListener('DOMContentLoaded', function(){
     // Attach an event listener to our 'updateButton.
     // When 'updateButton' receives a 'click' event, the 'handleButtonClick' function will execute.
     addItemButton.addEventListener('click', handleButtonClick);
-    totalDisplayElement.textContent = `Welcome, ${userName}! Click the button below to start counting`    
+    totalDisplayElement.textContent = `Welcome, ${totalCost}! Click the button below to start counting`    
 })
