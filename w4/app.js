d3b8c4f4d79eaa2452ccbed64e7fba88bff540a3
Chase Olisa <chaseolisa@gmail.com>
2026-02-05 04:32:51 -0800

    Step 3 Complete

--- w4/app.js
+++ w4/app.js
@@ -1,12 +1,15 @@
 //Imports this file to the other orderhandler
+//Imports this file to the price calculator
 import * as orderHandler from "./order-handler.js";
 import * as priceCalculator from "./price-calculator.js";
+
+const orders = [];
 
 //Reference to the Form
 const orderForm = document.getElementById('order-form');
 
 //References the Order Summary
-const orderSummary = document.getElementById('order-summary');
+//const orderSummary = document.getElementById('order-summary');
 
 //Handles the submit roder function
 const handleFormSubmit = function (event) {
@@ -17,14 +20,29 @@ const handleFormSubmit = function (event) {
     const orderData = orderHandler.getOrderInputs();
 
     //Update the Page to show order details
-    orderSummary.textContent = `You Ordered ${orderData.qty} ${orderData.size} Shirt(s)${orderData.giftWrap ? ' with Premium Wrap!': ''}`;
+    // orderSummary.textContent = `You Ordered ${orderData.qty} ${orderData.size} Shirt(s)${orderData.giftWrap ? ' with Premium Wrap!': ''}`;
     //console.log to return object literal
-    console.log(`Order Input - Object Literal:`);
-    console.log(`key of qty value of ${orderData.qty}`);
-    console.log(`key of size value of ${orderData.size}`);
-    console.log(`key of giftWrap value of ${orderData.giftWrap}`);
-    console.log(orderData);
+
+    const calculatedPrice = priceCalculator.calculateTotal(orderData);
+
+    const newOrder = {
+        ...orderData,
+        ...calculatedPrice,
+        timestamp: new Date().toISOString()
+    };
+    // To store in the list
+    orders.push(newOrder);
+
+    console.log(orders);
+    orderHandler.clearOrderForm();
 };
+
+//     console.log(`Order Input - Object Literal:`);
+//     console.log(`key of qty value of ${orderData.qty}`);
+//     console.log(`key of size value of ${orderData.size}`);
+//     console.log(`key of giftWrap value of ${orderData.giftWrap}`);
+//     console.log(orderData);
+// };
 
 
     //Set up the init function with Event Listeners
